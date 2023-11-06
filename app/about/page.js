8d555599cc00d93d0447c1ac980ae41e40417a08
Vladan <57448109+vladan345@users.noreply.github.com>
2023-11-06 10:36:04 +0100

    update caching (#1)
    Co-authored-by: v-nulapps <vladan@nulapps.nl>

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      about
+      <Link href="/">Back to home</Link>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -6,17 +6,26 @@ export default async function Home() {
   const blogs = await getBlogs();
 
   return (
-    <div className="p-20">
+    <div className="p-20 max-w-[1400px] mx-auto">
       <h1 className="text-[64px] mb-20 mt-20">Blogs</h1>
+      <Link href="/about">About us</Link>
       <div className="grid-cols-3 grid gap-10">
         {blogs.map((blog) => (
           <div
-            className="bg-slate-800 p-4 rounded-md min-h-[400px]"
+            className="bg-slate-800 rounded-[12px] min-h-[400px]"
             key={blog._id}
           >
-            <Image src={blog.image} alt="" width={800} height={500} />
-            <h2 className="text-[24px] my-4">{blog.title}</h2>
-            <span>{blog._createdAt.split("T").join(" ").split("Z")}</span>
+            <Image
+              className="rounded-t-[12px]"
+              src={blog.image}
+              alt=""
+              width={800}
+              height={500}
+            />
+            <div className="p-4">
+              <h2 className="text-[24px] my-4">{blog.title}</h2>
+              <span>{blog._createdAt.split("T").join(" ").split("Z")}</span>
+            </div>
           </div>
         ))}
       </div>
